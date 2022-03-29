cf48fddd3e3c4ed29dfda8211a112c6a8f6a3a41
Ben Armstead <contact@benarmstead.co.uk>
2022-03-29 11:44:01 +0100

    Add Arch install script

---
+++ install.sh
@@ -0,0 +1,88 @@
+# Pre-install
+# curl https://dots.benarmstead.co.uk/setup.sh > setup.sh
+# chmod +x setup.sh
+
+# Post-install
+# curl https://dots.benarmstead.co.uk/install.sh > install.sh
+# chmod +x install.sh
+
+username=$1
+# Hard code username for convenience
+username=ben
+
+# Go to tmp
+cd /tmp/
+
+# Install
+## Install official
+curl https://dots.benarmstead.co.uk/install/packages.txt > packages.txt
+sudo pacman -S - < packages.txt --noconfirm
+
+## Install third party
+### Ungoogled chromium
+curl -s 'https://download.opensuse.org/repositories/home:/ungoogled_chromium/Arch/x86_64/home_ungoogled_chromium_Arch.key' | sudo pacman-key -a -
+echo '
+[home_ungoogled_chromium_Arch]
+SigLevel = Required TrustAll
+Server = https://download.opensuse.org/repositories/home:/ungoogled_chromium/Arch/$arch' | sudo tee --append /etc/pacman.conf
+sudo pacman -Sy
+
+### Yay
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -i --noconfirm
+cd ..
+
+### AUR
+yay -S lf-bin --noconfirm
+yay -S tutanota-desktop-bin --noconfirm
+yay -S vscodium-bin --noconfirm
+yay -S lf-bin --noconfirm
+
+### Codium extensions
+#codium --list-extensions
+curl https://dots.benarmstead.co.uk/install/codium_extensions.txt > codium_extensions.txt
+cat codium_extensions.txt | xargs -n 1 codium --install-extension
+
+
+# Configurations
+## Dots
+git clone https://github.com/benarmstead/dots.git
+mkdir -p ~/.config
+chmod +x dots/lf/*
+mv dots/* ~/.config/
+
+## Git
+git config --global credential.helper store
+
+## Sudo
+sudo echo "root ALL=(ALL:ALL) ALL
+$username ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers
+
+## Bluetooth
+modprobe btusb
+sudo systemctl enable bluetooth
+
+## Networking
+sudo systemctl enable NetworkManager
+
+## Rust
+rustup install stable
+rustup default stable
+
+## Neovim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+nvim -c 'PlugInstall | PlugUpdate | :q! | :q!'
+
+## ZSH
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+mv ~/.config/zshrc ~/.
+echo 'alias lf=~/.config/lf/lfub
+export EDITOR=nvim' >> ~/.zshrc
+
+## Sway
+echo 'if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
+  exec sway
+fi' >> ~/.profile
+
+#sudo reboot
---
+++ setup.sh
@@ -0,0 +1,3 @@
+# curl https://dots.benarmstead.co.uk/setup.sh > install.sh
+# chmod +x install.sh
+archinstall --config https://dots.benarmstead.co.uk/install/install.json --disk_layouts https://dots.benarmstead.co.uk/install/disk_layout.json
