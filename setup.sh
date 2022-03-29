e3b5b9e5a732de6942d9a0266201835317690c46
Ben Armstead <contact@benarmstead.co.uk>
2022-03-29 16:39:39 +0100

    update

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-# curl https://dots.benarmstead.co.uk/setup.sh > install.sh
-# chmod +x install.sh
-archinstall --config https://dots.benarmstead.co.uk/install/install.json --disk_layouts https://dots.benarmstead.co.uk/install/disk_layout.json
+# curl https://dots.benarmstead.co.uk/setup.sh > setup.sh
+# chmod +x setup.sh
+archinstall --config https://dots.benarmstead.co.uk/install/install.json #--disk_layouts https://dots.benarmstead.co.uk/install/disk_layout.json
