27025d08f1e10f9cdb32e1aca3d2aad09d8a9e76
Ben Armstead <ben@benarmstead.co.uk>
2022-11-16 19:20:15 +0000

    Update dots

--- install/install.sh
+++ install/install.sh
@@ -7,33 +7,22 @@ git_email=$2
 git_name=$3
 # Hard code username for convenience
 #username=ben
-#git_email="contact@benarmstead.co.uk"
+#git_email="ben@benarmstead.co.uk"
 #git_name="Ben Armstead"
 
 # Go to tmp
 cd /tmp/
 
 # Install
-
-## Install official
-curl https://dots.benarmstead.co.uk/install/packages.txt > packages.txt
-sudo pacman -S - < packages.txt --noconfirm
-
-### Pip
-pip3 install virtualenv
-pip3 install django
-
 ### Yay
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -i --noconfirm
 cd ..
 
-### AUR
-yay -S lf-bin --noconfirm
-yay -S vscodium-bin --noconfirm
-yay -S nerd-fonts-complete --noconfirm
-yay -S ungoogled-chromium-bin --noconfirm
+## Install packages
+curl https://dots.benarmstead.co.uk/install/packages.txt > packages.txt
+yay -S - < packages.txt --noconfirm
 
 ### Codium extensions
 #codium --list-extensions
@@ -70,14 +59,10 @@ rustup install stable
 rustup default stable
 
 ## Neovim
-mv ~/.config/nvim/ ~/.config/nvim-save
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
-mv ~/.config/nvim-save ~/.config/nvim
+git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+nvim +PackerSync
 
 ## ZSH
-#yay -S --noconfirm zsh-theme-powerlevel10k-git
-#echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-#sudo chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 curl https://raw.githubusercontent.com/kyoz/purify/master/zsh/purify.zsh-theme --output ~/.oh-my-zsh/themes/purify.zsh-theme
 cp ~/.config/zshrc ~/.zshrc
